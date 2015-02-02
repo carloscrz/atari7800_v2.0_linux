ee256c836b4e1aabd8620341f24d5a4f957c8153
carloscrz <olliv3r86@yahoo.com.br>
2015-02-02 16:27:28 -0500

    Update kbd.c

--- kbd.c
+++ kbd.c
@@ -1,4 +1,5 @@
 /*
+ * modified in 01/2015, by carlos
  *
  *  Copyright (C) 2009 Ludovic Jacomme (ludovic.jacomme@gmail.com)
  *  This program is free software; you can redistribute it and/or modify
