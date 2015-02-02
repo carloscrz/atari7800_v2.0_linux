01435ec7621d47d5a8d40450ed809b9d62c9f3dd
carloscrz <olliv3r86@yahoo.com.br>
2015-02-02 16:28:48 -0500

    Update input.c

--- input.c
+++ input.c
@@ -1,5 +1,5 @@
 /*
- *  modified in 01/2015, by carlos
+ *  modified in 01/2015, by carloscrz
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
